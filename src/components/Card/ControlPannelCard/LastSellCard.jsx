67e785b4f7f7ea7d72ea19a1b76fc3a4a86846a0
Fellipe Lorram <fellipelorram@gmail.com>
2022-01-24 16:54:02 -0300

    Changing control pannel cards

--- src/components/Card/ControlPannelCard/LastSellCard.jsx
+++ src/components/Card/ControlPannelCard/LastSellCard.jsx
@@ -19,45 +19,47 @@ const cardVariants = {
 
 function LastSellCardComponent({ data }) {
   const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
-  const content = {
+  const format = {
     id: data._id,
     nome: data.nome,
     data: new Date(data.sell.CriadoEm).toLocaleDateString('pt-BR', options),
-    armacao: data.sell.armacao || 'N/A',
     valorArm: data.sell.valorArm.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }) || '0,00',
     lente: data.sell.lente || 'N/A',
     valorLen: data.sell.valorLen.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }) || '0,00',
-    total: data.sell.total || '0,00',
     pago: data.sell.pago.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }) || '0,00',
     resta: data.sell.resta.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }) || '0,00',
-    sellId: data.sell._id,
   };
+
   return (
     <Container
       variants={cardVariants}
       initial="initial"
       animate="animate"
-      onClick={() => history.push(`/edit-sell/${content.id}/${content.sellId}`)}
+      onClick={() => history.push(`/edit-sell/${data._id}/${data.sell._id}`)}
     >
       <div className="content">
-        <span>{content.nome}</span>
-        <span>{content.data}</span>
+        <span>{data.nome}</span>
+        <span>{format.data}</span>
       </div>
       <div className="content">
-        <span>{content.armacao}</span>
-        <CurrencyFormat value={content.valorArm} displayType="text" thousandSeparator prefix="R$" />
+        <span>{data.sell.armacao || 'N/A'}</span>
+        <CurrencyFormat value={format.valorArm} displayType="text" thousandSeparator prefix="R$" />
       </div>
       <div className="content">
-        <span>{content.lente}</span>
-        <CurrencyFormat value={content.valorLen} displayType="text" thousandSeparator prefix="R$" />
+        <span>{format.lente}</span>
+        <CurrencyFormat value={data.sell.valorLen || 'N/A'} displayType="text" thousandSeparator prefix="R$" />
       </div>
       <div className="content">
         <span>Total</span>
-        <CurrencyFormat value={content.total} displayType="text" thousandSeparator prefix="R$" />
+        <CurrencyFormat value={data.sell.total || '0,00'} displayType="text" thousandSeparator prefix="R$" />
       </div>
       <div className="content">
-        <span>{content.pago}</span>
-        <CurrencyFormat value={content.resta} displayType="text" thousandSeparator prefix="R$" />
+        <span>Valor pago:</span>
+        <CurrencyFormat value={format.pago} displayType="text" thousandSeparator prefix="R$" />
+      </div>
+      <div className="content">
+        <span>Valor restante:</span>
+        <CurrencyFormat value={format.resta} displayType="text" thousandSeparator prefix="R$" />
       </div>
     </Container>
   );
